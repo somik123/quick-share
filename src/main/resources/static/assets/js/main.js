eeab1282279252b0ac8bd26ae2d476fcb24b7f4f
Somik <somik123@users.noreply.github.com>
2025-01-04 18:00:00 +0800

    Fix regex

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -145,17 +145,13 @@ function lengthCounter() {
 function linkify(inputText) {
     var replacedText, replacePattern1, replacePattern2, replacePattern3;
 
-    //URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(^|[^\]])(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    //URLs starting with http://, https://, ftp:// or www
+    replacePattern1 = /((?<!\])\b(?:https?|ftp):\/\/[^\s\/$.?#].[^\s]*\b|\bwww\.[^\s\/$.?#].[^\s]*\b)/gi;
     replacedText = inputText.replaceAll(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replaceAll(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
-
     //Change email addresses to mailto:: links.
-    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replaceAll(replacePattern3, '<a href="mailto:$1">$1</a>');
+    replacePattern2 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replaceAll(replacePattern2, '<a href="mailto:$1">$1</a>');
 
     var src_arr = ["\n", "[b]", "[/b]", "[i]", "[/i]", "[img]", "[/img]"];
     var rep_arr = ["<br>", "<strong>", "</strong>", "<em>", "</em>", "<img src=\"", "\" alt=\"image\" />"];
